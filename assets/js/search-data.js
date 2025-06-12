9cff8d5fef09797ca04680e4db7145a5ffe11aaa
yuzhe-chen <yuzhechen2011@gmail.com>
2025-06-12 14:52:35 +0000

    Deploying to gh-pages from @ yuzhe-chen/yuzhe-chen.github.io@d81ccb56b5e00de3f790a9ce58ab7bef5cad4a9d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-farting",
-    title: "farting",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
