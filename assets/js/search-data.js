9f345a5952fec7e2ac73dcc4320bb20940d47ce6
yuzhe-chen <yuzhechen2011@gmail.com>
2025-07-24 22:50:46 +0000

    Deploying to gh-pages from @ yuzhe-chen/yuzhe-chen.github.io@6fb35b289c46e4cdfc1e36c5f2dfd8697feadea8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,7 +31,7 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "nav-awards",
-          title: "Awards",
+          title: "awards",
           description: "",
           section: "Navigation",
           handler: () => {
