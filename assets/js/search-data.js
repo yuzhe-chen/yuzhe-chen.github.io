45a73ea92152132497c85d01294ba868f9008976
yuzhe-chen <yuzhechen2011@gmail.com>
2025-07-24 22:51:35 +0000

    Deploying to gh-pages from @ yuzhe-chen/yuzhe-chen.github.io@5652a71af87e8f8a13cbb2409aae0ffbd369e221 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/awards/";
           },
+        },{id: "nav-bookshelf",
+          title: "bookshelf",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/books/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
