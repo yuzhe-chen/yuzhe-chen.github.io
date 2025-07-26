a05c4e2ecd94a859601ea76197cfab0677018a4d
yuzhe-chen <yuzhechen2011@gmail.com>
2025-07-26 17:58:30 +0000

    Deploying to gh-pages from @ yuzhe-chen/yuzhe-chen.github.io@4b59b08cdc7d5c63ee54608b645b4416c4be7354 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -480,7 +480,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%79%75%7A%68%65%63%68%65%6E%32%30%31%31@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -502,6 +502,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-youtube',
+        title: 'YouTube',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://youtube.com/@youtube.com/@espressoofjoy--music0411", "_blank");
         },
       },{
         id: 'social-custom_social',
