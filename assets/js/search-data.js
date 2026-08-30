5a9185e8f53adde190308fbdd8d597572bfaa8c0
yuzhe-chen <yuzhechen2011@gmail.com>
2026-08-30 01:41:03 +0000

    Deploying to gh-pages from @ yuzhe-chen/yuzhe-chen.github.io@5f5b542ea3a2ff0a525f313c0d274970923d4f25 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -34,17 +34,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{
+          section: "News",},{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
