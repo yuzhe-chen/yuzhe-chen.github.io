d0a9a16b8fbfba94ca51d541ac6608e4f2da04e4
yuzhe-chen <yuzhechen2011@gmail.com>
2026-08-30 01:37:40 +0000

    Deploying to gh-pages from @ yuzhe-chen/yuzhe-chen.github.io@b75cd645428467d0702d74feeecc7a0e2d284504 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -54,6 +54,11 @@ ninja.data = [{
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
